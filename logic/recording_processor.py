a89d474fd3569a48aef6c91618cb23a13f8d9fcf
Aleksas Vaitulevicius <aleksas.vaitulevicius@euromonitor.com>
2019-10-30 18:57:36 +0200

    recording processing

--- logic/recording_processor.py
+++ logic/recording_processor.py
@@ -1,17 +1,31 @@
 from concurrent.futures.thread import ThreadPoolExecutor
-from acrcloud import ACRcloud
+from acrcloud.recognizer import ACRCloudRecognizer
+from ibm_watson import SpeechToTextV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import json
 
 SLAVES = 5
-re = ACRcloud
-re.host = 'identify-eu-west-1.acrcloud.com'
-re.key = '9e9a124e292b7d0161e4ea997fff8f6b'
-re.secret = 'fLtsJPamnjz6nlCiTO7aaXivZR4VYxNSgFOu4V8e'
+re = ACRCloudRecognizer({
+	'host': 'identify-eu-west-1.acrcloud.com',
+	'access_key': '9e9a124e292b7d0161e4ea997fff8f6b',
+	'access_secret': 'fLtsJPamnjz6nlCiTO7aaXivZR4VYxNSgFOu4V8e',
+	'timeout': 10  # seconds
+})
+ibm_auth = IAMAuthenticator('s4Od7pNSAaRJbDVJnck1P3R3OonJzdhK11RGYuDOYZPl')
+stt = SpeechToTextV1(authenticator=ibm_auth)
+stt.set_service_url('https://gateway-lon.watsonplatform.net/speech-to-text/api')
 
 
 def slave(recs):
-	print(recs[1])
 	return {
-		recs[0]: re.recognizer(recs[1], recs[1])
+		recs[0]: {
+			'music': json.loads(re.recognize_by_filebuffer(recs[1].read(), 0, 600)),
+			'speech': stt.recognize(
+				recs[1],
+				content_type=recs[1].content_type,
+				word_alternatives_threshold=0.9
+			).get_result()
+		}
 	}
 
 
--- logic/text_processor.py
+++ logic/text_processor.py
@@ -1,11 +1,18 @@
 from concurrent.futures.thread import ThreadPoolExecutor
 
 import detectlanguage as dl
+from ibm_watson import ToneAnalyzerV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 dl.configuration.api_key = '1f64cbddd63bd66c8f0052d072d2c36e'
 
+ibm_auth = IAMAuthenticator('pIHe8W33C4luJ58-HyRkkJ3EriuazRxdU-eZJ0T__eNa')
+ta = ToneAnalyzerV3(authenticator=ibm_auth, version='2017-09-21')
+ta.set_service_url('https://gateway-lon.watsonplatform.net/tone-analyzer/api')
+
 ACTION_LIST = [
 	# ('language_detection', lambda contents: detect_languages(contents)),
+	('tone_analysis', lambda contents: ta.tone(contents)),
 	('test_text', lambda contents: ['text' for _ in contents]),
 ]
 N_SLAVES = 2
