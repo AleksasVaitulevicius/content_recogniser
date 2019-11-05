a47b50a410d2348428814b0f73f2d14a52a9947a
Aleksas <aleksas1995@gmail.com>
2019-11-06 00:55:19 +0200

    new image processing

--- logic/image_processor.py
+++ logic/image_processor.py
@@ -1,111 +1,29 @@
 from concurrent.futures.thread import ThreadPoolExecutor
-from time import sleep
-from config import Imagga
-import requests
-import json
+
+from ibm_watson import VisualRecognitionV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from config import WatsonVR
 
 N_SLAVES = 5
 
-BATCH_TEMPLATE = {
-	'/tags': [],
-	'/text': [],
-	'/colors': [],
-}
+authenticator = IAMAuthenticator(WatsonVR.API_KEY)
+visual_recognition = VisualRecognitionV3(version='2018-03-19', authenticator=authenticator)
+visual_recognition.set_service_url(WatsonVR.SERVICE_URL)
 
 
-def send_request(func, retries, url, json_data, extract_by_keys, return_on_err, **kwargs):
-	for try_no in range(1, retries + 1):
-		try:
-			if func == requests.get:
-				result = requests.get(url=url, **kwargs)
-			else:
-				result = func(url, None, json_data, **kwargs)
-			if not result:
-				print(result.content)
-				raise Exception(result.reason + ': ' + str(result.status_code))
-			result = result.json()['result']
-			for key in extract_by_keys:
-				result = result[key]
-			return result
-		except Exception as e:
-			print('exception:', e)
-			continue
-	return return_on_err
-
-
-def upload_image(image):
-	return send_request(
-		requests.post, 1, Imagga.UPLOAD_URL, None, ['upload_id'], None,
-		auth=(Imagga.API_KEY, Imagga.API_SECRET), files={'image': image}
-	)
-
-
-def upload_in_parallel(images):
-	with ThreadPoolExecutor(N_SLAVES) as executor:
-		future = executor.map(upload_image, images)
-	if len(list(future)) == 0:
-		raise Exception('Error occurred while uploading image')
-	return future
-
-
-def prepare_batch_param(img_id):
-	return {
-		'params': {
-			'image_upload_id': img_id
+def send_request(key, image):
+	try:
+		return {
+			key: visual_recognition.classify(
+				images_file=image
+			).get_result()
 		}
-	}
-
-
-def get_ticket_result(ticket):
-	response = {'is_final': False}
-	while isinstance(response, dict) and not response['is_final']:
-		sleep(5)
-		response = send_request(
-			requests.get, 1, Imagga.TICKET_URL + ticket, None, [], 'error occurred while getting ticket',
-			auth=(Imagga.API_KEY, Imagga.API_SECRET)
-		)
-	if isinstance(response, str):
-		return response
-	return response['ticket_result']['final_result']
-
-
-def submit_batch(img_ids):
-	params = [prepare_batch_param(img_id) for img_id in img_ids]
-	body = {endpoint: params for endpoint in BATCH_TEMPLATE}
-	ticket = send_request(
-		requests.post, 1, Imagga.BATCH_URL, body, ['ticket_id'], 'error occurred while submitting batch',
-		auth=(Imagga.API_KEY, Imagga.API_SECRET)
-	)
-	if ticket == 'error occurred while submitting batch':
-		raise Exception('error occurred while submitting batch')
-	return get_ticket_result(ticket)
-
-
-def extract_data(keys, results, unloaded_img_ids):
-	extracted = {key: BATCH_TEMPLATE.copy() for key in keys}
-	for result_key in BATCH_TEMPLATE.keys():
-		for key, result in zip(keys, results[result_key]):
-			if key in unloaded_img_ids:
-				extracted[key][result_key] = 'error occurred while uploading image'
-				continue
-			if 'result' in result.keys() and 'ticket_id' in result['result'].keys():
-				result = get_ticket_result(result['result']['ticket_id'])
-			extracted[key][result_key] = result
-	return extracted
+	except Exception as e:
+		print(e)
+		return {key: str(e)}
 
 
 def process(images):
-	keys = images.keys()
-	if len(keys) == 0:
-		return {}
-	contents = images.values()
-	img_ids = upload_in_parallel(contents)
-	# with open('img_ids.json', 'r') as img_ids_file:
-	# 	img_ids = json.load(img_ids_file)
-	unloaded_img_ids = [img_id for key, img_id in zip(keys, img_ids) if img_id is None]
-	img_ids = [img_id for img_id in img_ids if img_id is not None]
-	try:
-		response = submit_batch(img_ids)
-	except Exception as e:
-		return {key: str(e) for key in keys}
-	return extract_data(keys, response, unloaded_img_ids)
+	with ThreadPoolExecutor(len(images.items())) as executor:
+		future = executor.map(send_request, images.keys(), images.values())
+	return {key: description for result in list(future) for key, description in result.items()}
--- logic/text_processor.py
+++ logic/text_processor.py
@@ -1,19 +1,15 @@
 from concurrent.futures.thread import ThreadPoolExecutor
 
-import detectlanguage as dl
 from ibm_watson import ToneAnalyzerV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-from config import WatsonTA, DetectLanguage
-
-dl.configuration.api_key = DetectLanguage.API_KEY
+from config import WatsonTA
 
 ibm_auth = IAMAuthenticator(WatsonTA.API_KEY)
 ta = ToneAnalyzerV3(authenticator=ibm_auth, version='2017-09-21')
 ta.set_service_url(WatsonTA.SERVICE_URL)
 
 ACTION_LIST = [
-	# ('language_detection', lambda contents: detect_languages(contents)),
-	# ('tone_analysis', lambda contents: detect_tone(contents)),
+	('tone_analysis', lambda contents: detect_tone(contents)),
 ]
 N_SLAVES = 2
 
@@ -23,13 +19,6 @@ def detect_tone(contents):
 		return [ta.tone(content).get_result() for content in contents]
 	except Exception as e:
 		print(e)
-		return "Error occurred"
-
-
-def detect_languages(contents):
-	try:
-		return [content[0] for content in dl.detect(contents)]
-	except Exception:
 		return "Error occurred"
 
 
